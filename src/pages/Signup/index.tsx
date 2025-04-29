8186c308f5bc96d3c93b924af580bf9dd7e09c62
Hayk Dallakyan <hayk.dallakyan@teamsolutions.am>
2025-04-29 15:10:30 +0400

    code review

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Signup:React.FC = () => {
-    // TODO make sign up page with small validations, don't use <form> element, and when there is an error in any field when unfocus, the error is triggered, when typing in the field , removethe  error
+    // TODO make sign up page with small validations, don't use <form> element, and when there is an error in any field when unfocus, the error is triggered, when typing in the field , remove the  error
     // registered when pressing the enter button and clicking sign-in button
     // sign in after registration with Promise.allWithMode , for example Promise.allWithMode([singupcall(), signincall()])
     return (
